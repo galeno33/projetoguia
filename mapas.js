da2887cb701136ad62148e88b1a2fbb95f154b43
galeno33 <48499860+galeno33@users.noreply.github.com>
2020-06-30 15:57:38 -0300

    Add files via upload

--- mapas.js
+++ mapas.js
@@ -17,6 +17,29 @@ function initMap() {
 
     var infoWindow = new google.maps.InfoWindow;
 
+    //gerar a posiçao atual do usuario
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+        
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+            }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+            }
+
+
+    
+
     downloadUrl('banco.php', function(data) {
         var xml = data.responseXML;
         var markers = xml.documentElement.getElementsByTagName('marker');
@@ -30,12 +53,15 @@ function initMap() {
                 parseFloat(markerElem.getAttribute('lat')), //buscar a latitude do banco.php
                 parseFloat(markerElem.getAttribute('lng'))); //buscar a longitude do banco.php
 
+            //nomear o marcador dando o tipo = cadeirante ou visual
             var infowincontent = document.createElement('div');
             var strong = document.createElement('strong');
             strong.textContent = tipo
             infowincontent.appendChild(strong);
             infowincontent.appendChild(document.createElement('br'));
+                        
 
+            //apresentar o endereço do marcador
             var text = document.createElement('text');
             text.textContent = endereco
             infowincontent.appendChild(text);
@@ -52,10 +78,23 @@ function initMap() {
                 infoWindow.open(map, marker); //marker
             });
         });
+                
     });
+    
 }
+    //codigo para adicionar aplicaçao de voz para deficiente auditivos
+    var msg = new SpeechSynthesisUtterance();
+    msg.infowincontent = text;
+    window.speechSynthesis.speak(msg);
 
-
+//funçao sobre localizaçao atual do usuario
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
 
 function downloadUrl(Url, callback) {
     var request = window.ActiveXObject ?
